5e8fac2cb404f4c810d10b672b0308d61941d412
rishisAryal <risharyal5@gmail.com>
2023-09-18 16:39:16 +0545

    init

---
+++ src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+        <div>Body</div>
+        <Footer/>
+      </div>
+
+    </div>
+  );
+}
